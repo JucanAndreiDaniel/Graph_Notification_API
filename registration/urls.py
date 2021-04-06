759a3fcf41790547bad9ae4d70b19673c0a7c728
Setrofex <setrofex@gmail.com>
2021-04-06 17:46:29 +0300

    django rebase

---
+++ registration/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
+from . import views
+
+urlpatterns = [
+    path('register',views.register, name="register"),
+    path('login',views.login, name="login"),
+    path('logout',views.logout, name="logout"),
+    path('hello/', views.HelloView.as_view(), name='hello'),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+]
---
+++ registration/views.py
@@ -0,0 +1,71 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User, auth
+from django.http import HttpResponse, HttpResponseRedirect
+# Rest-framework
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+# Create your views here.
+
+#http http://127.0.0.1:8000/registration/hello/ "Authorization: Token 550df1a082c1a5ff6e6634a5a1cab42e5ebd3098"
+
+class HelloView(APIView):
+    permission_classes = (IsAuthenticated,)
+    def get(self, request):
+        content = {'message': 'Hello, World!'}
+        return Response(content)
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            if user.is_active:
+                auth.login(request, user)
+
+                return HttpResponseRedirect('/')
+            else:
+                return HttpResponse("Inactive user.")
+        else:
+            return HttpResponseRedirect('login')
+    return render(request, "login.html")
+
+
+def register(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        user_name = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if password1 == password2:
+            if User.objects.filter(username=user_name).exists():
+                messages.info(request, 'Username Taken')
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Taken')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(
+                    username=user_name, password=password1, email=email, first_name=first_name, last_name=last_name)
+                user.save()
+                print('user created')
+                return redirect('register')
+
+        else:
+            messages.info(request, 'password not matching..')
+            return redirect('register')
+        return redirect('/')
+
+    else:
+        return render(request, 'register.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
