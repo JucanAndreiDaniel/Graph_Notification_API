a7cd780576142a3ac0dca5b381ac6adf1e518b94
Setrofex <setrofex@gmail.com>
2021-04-06 18:07:04 +0300

    urls import sort

--- login_api/urls.py
+++ login_api/urls.py
@@ -14,15 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls import include, url
+from django.conf.urls import include
 from django.urls import path
 from django.views.generic.base import TemplateView
-from django.contrib.auth import views as auth_views
-from registration import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('accounts/', include('django.contrib.auth.urls')),
-    path('registration/',include('registration.urls')),
+    path('registration/', include('registration.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
