1cbbd55294feac9ffbd81ed0c69953a2cf274750
Setrofex <setrofex@gmail.com>
2021-05-18 18:23:10 +0300

    add hourly values for coins for the last week

--- registration/models.py
+++ registration/models.py
@@ -18,6 +18,9 @@ class value(models.Model):
     coin = models.ForeignKey(cryptoObject, on_delete=models.CASCADE)
     currency = models.CharField(max_length=255)
 
+    percentage_1h = models.FloatField(blank=True, null=True)
+    percentage_1d = models.FloatField(blank=True, null=True)
+
     current = models.FloatField(blank=True, null=True)
     high_1d = models.FloatField(blank=True, null=True)
     low_1d = models.FloatField(blank=True, null=True)
@@ -27,6 +30,50 @@ class value(models.Model):
 
     atl = models.FloatField(blank=True, null=True)
     atl_time = models.DateTimeField(7)
+
+
+class market_chart(models.Model):
+    coin_currency_day = models.CharField(
+        max_length=255, primary_key=True)
+    coin = models.ForeignKey(cryptoObject, on_delete=models.CASCADE)
+    currency = models.CharField(max_length=255)
+
+    class Days(models.TextChoices):
+        ONE = '1', ('1')
+        TWO = '2', ('2')
+        THREE = '3', ('3')
+        FOUR = '4', ('4')
+        FIVE = '5', ('5')
+        SIX = '6', ('6')
+        SEVEN = '7', ('7')
+
+    day = models.CharField(
+        max_length=255, choices=Days.choices, default=Days.ONE)
+
+    price1 = models.FloatField(blank=True, null=True)
+    price2 = models.FloatField(blank=True, null=True)
+    price3 = models.FloatField(blank=True, null=True)
+    price4 = models.FloatField(blank=True, null=True)
+    price5 = models.FloatField(blank=True, null=True)
+    price6 = models.FloatField(blank=True, null=True)
+    price7 = models.FloatField(blank=True, null=True)
+    price8 = models.FloatField(blank=True, null=True)
+    price9 = models.FloatField(blank=True, null=True)
+    price10 = models.FloatField(blank=True, null=True)
+    price11 = models.FloatField(blank=True, null=True)
+    price12 = models.FloatField(blank=True, null=True)
+    price13 = models.FloatField(blank=True, null=True)
+    price14 = models.FloatField(blank=True, null=True)
+    price15 = models.FloatField(blank=True, null=True)
+    price16 = models.FloatField(blank=True, null=True)
+    price17 = models.FloatField(blank=True, null=True)
+    price18 = models.FloatField(blank=True, null=True)
+    price19 = models.FloatField(blank=True, null=True)
+    price20 = models.FloatField(blank=True, null=True)
+    price21 = models.FloatField(blank=True, null=True)
+    price22 = models.FloatField(blank=True, null=True)
+    price23 = models.FloatField(blank=True, null=True)
+    price24 = models.FloatField(blank=True, null=True)
 
 
 class Notification(models.Model):
--- updatedb.py
+++ updatedb.py
@@ -1,10 +1,14 @@
-import traceback
-import sys
+
 from pycoingecko import CoinGeckoAPI
-import os
+import time
+import datetime
+import traceback
 import django
+import os
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'login_api.settings')
+
+
 django.setup()
 
 
@@ -13,26 +17,10 @@ cg = CoinGeckoAPI()
 currencies = ["usd", "eur", "gbp", "rub"]
 
 
-def progressbar(it, prefix="", size=60, file=sys.stdout):
-    count = len(it)
-
-    def show(j):
-        x = int(size*j/count)
-        file.write("%s[%s%s] %i/%i\r" %
-                   (prefix, "#"*x, "."*(size-x), j, count))
-        file.flush()
-    show(0)
-    for i, item in enumerate(it):
-        yield item
-        show(i+1)
-    file.write("\n")
-    file.flush()
-
-
 def correcttime(timestamp: str):
     try:
         timestamplist = timestamp.split("T")
-        correct = timestamplist[0]+" "+timestamplist[1][:-1]+"+00:00"
+        correct = timestamplist[0]+" "+timestamplist[1][:-2]+"+00:00"
     except:
         correct = "2000-01-01 00:00:00+00:00"
         print(f"{timestamp}\n")
@@ -40,12 +28,14 @@ def correcttime(timestamp: str):
 
 
 try:
+    start = time.time()
     import registration.models as mod
-    for i in progressbar(range(1, 26)):
+    from registration.models import market_chart
+    for i in range(1, 26):
         price_list = []
         for curr in currencies:
             price = cg.get_coins_markets(
-                vs_currency=curr, per_page="250", page=f"{i}")
+                vs_currency=curr, per_page="250", page=f"{i}", sparkline="true", price_change_percentage="1h")
             price_list += price
         for j in range(250):
             cryptoObject = mod.cryptoObject(coin_id=price_list[j]["id"], symbol=price_list[j]["symbol"],
@@ -53,15 +43,84 @@ try:
                                             last_updated=correcttime(price_list[j]["last_updated"]))
             cryptoObject.save()
             for k in range(4):
-                crypto_value = mod.value(coin_currency=f"{price_list[j]['id']}_{currencies[k]}", coin=cryptoObject,
-                                         currency=currencies[k], current=price_list[j+(
-                                             250*k)]["current_price"],
-                                         high_1d=price_list[j+(250*k)]["high_24h"], low_1d=price_list[j+(
-                                             250*k)]["low_24h"],
-                                         ath=price_list[j+(250*k)]["ath"], ath_time=correcttime(
-                                             price_list[j+(250*k)]["ath_date"]),
-                                         atl=price_list[j+(250*k)]["ath"], atl_time=correcttime(price_list[j+(250*k)]["atl_date"]))
+                coinFromList = price_list[j+(250*k)]
+                crypto_value = mod.value(coin_currency=f"{price_list[j]['id']}_{currencies[k]}",
+                                         coin=cryptoObject,
+                                         currency=currencies[k],
+                                         current=coinFromList["current_price"],
+                                         high_1d=coinFromList["high_24h"],
+                                         low_1d=coinFromList["low_24h"],
+                                         ath=coinFromList["ath"],
+                                         ath_time=correcttime(
+                                             coinFromList["ath_date"]),
+                                         atl=coinFromList["ath"],
+                                         atl_time=correcttime(
+                                             coinFromList["atl_date"]),
+                                         percentage_1d=coinFromList["price_change_percentage_24h"],
+                                         percentage_1h=coinFromList["price_change_percentage_24h"])
                 crypto_value.save()
+                for l in range(1, 8):
+                    try:
+                        crypto_market = mod.market_chart(coin_currency_day=f"{price_list[j]['id']}_{currencies[k]}_{(l-1)}",
+                                                         coin=cryptoObject,
+                                                         currency=currencies[k],
+                                                         day=market_chart.Days(
+                                                             f"{l}"),
+                                                         price1=coinFromList["sparkline_in_7d"]["price"][0+(
+                                                             24*(int(market_chart.Days(f"{l}").value)-2))],
+                                                         price2=coinFromList["sparkline_in_7d"]["price"][1+(
+                                                             24*(int(market_chart.Days(f"{l}").value)-2))],
+                                                         price3=coinFromList["sparkline_in_7d"]["price"][2+(
+                                                             24*(int(market_chart.Days(f"{l}").value)-2))],
+                                                         price4=coinFromList["sparkline_in_7d"]["price"][3+(
+                                                             24*(int(market_chart.Days(f"{l}").value)-2))],
+                                                         price5=coinFromList["sparkline_in_7d"]["price"][4+(
+                                                             24*(int(market_chart.Days(f"{l}").value)-2))],
+                                                         price6=coinFromList["sparkline_in_7d"]["price"][5+(
+                                                             24*(int(market_chart.Days(f"{l}").value)-2))],
+                                                         price7=coinFromList["sparkline_in_7d"]["price"][6+(
+                                                             24*(int(market_chart.Days(f"{l}").value)-2))],
+                                                         price8=coinFromList["sparkline_in_7d"]["price"][7+(
+                                                             24*(int(market_chart.Days(f"{l}").value)-2))],
+                                                         price9=coinFromList["sparkline_in_7d"]["price"][8+(
+                                                             24*(int(market_chart.Days(f"{l}").value)-2))],
+                                                         price10=coinFromList["sparkline_in_7d"]["price"][9+(
+                                                             24*(int(market_chart.Days(f"{l}").value)-2))],
+                                                         price11=coinFromList["sparkline_in_7d"]["price"][10+(
+                                                             24*(int(market_chart.Days(f"{l}").value)-2))],
+                                                         price12=coinFromList["sparkline_in_7d"]["price"][11+(
+                                                             24*(int(market_chart.Days(f"{l}").value)-2))],
+                                                         price13=coinFromList["sparkline_in_7d"]["price"][12+(
+                                                             24*(int(market_chart.Days(f"{l}").value)-2))],
+                                                         price14=coinFromList["sparkline_in_7d"]["price"][13+(
+                                                             24*(int(market_chart.Days(f"{l}").value)-2))],
+                                                         price15=coinFromList["sparkline_in_7d"]["price"][14+(
+                                                             24*(int(market_chart.Days(f"{l}").value)-2))],
+                                                         price16=coinFromList["sparkline_in_7d"]["price"][15+(
+                                                             24*(int(market_chart.Days(f"{l}").value)-2))],
+                                                         price17=coinFromList["sparkline_in_7d"]["price"][16+(
+                                                             24*(int(market_chart.Days(f"{l}").value)-2))],
+                                                         price18=coinFromList["sparkline_in_7d"]["price"][17+(
+                                                             24*(int(market_chart.Days(f"{l}").value)-2))],
+                                                         price19=coinFromList["sparkline_in_7d"]["price"][18+(
+                                                             24*(int(market_chart.Days(f"{l}").value)-2))],
+                                                         price20=coinFromList["sparkline_in_7d"]["price"][19+(
+                                                             24*(int(market_chart.Days(f"{l}").value)-2))],
+                                                         price21=coinFromList["sparkline_in_7d"]["price"][20+(
+                                                             24*(int(market_chart.Days(f"{l}").value)-2))],
+                                                         price22=coinFromList["sparkline_in_7d"]["price"][21+(
+                                                             24*(int(market_chart.Days(f"{l}").value)-2))],
+                                                         price23=coinFromList["sparkline_in_7d"]["price"][22+(
+                                                             24*(int(market_chart.Days(f"{l}").value)-2))],
+                                                         price24=coinFromList["sparkline_in_7d"]["price"][23+(
+                                                             24*(int(market_chart.Days(f"{l}").value)-2))],
+                                                         )
+                    except Exception as e:
+                        pass
+                    finally:
+                        crypto_market.save()
 
 except:
     traceback.print_exc()
+finally:
+    print(f"{datetime.datetime.now()} {round(time.time()-start, 2)}")
